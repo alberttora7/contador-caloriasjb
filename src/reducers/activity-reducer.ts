2c64dc33115f20ba38dd08ada0f5a335c5053100
Jose Rodriguez <alberttora7@gmail.com>
2026-04-04 20:06:26 -0400

    localStorage configurado para mantenre la info aun se recarge

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -13,8 +13,12 @@ export type ActivityState = {
     activeId: Activity['id']
 }
 
+const localStorageActivities = () : Activity[] => {
+    const activities = localStorage.getItem('activities')
+    return activities ? JSON.parse(activities) : []
+}
 export const initialState : ActivityState = {
-    activities: [],
+    activities: localStorageActivities(),
     activeId:''
 }
 
